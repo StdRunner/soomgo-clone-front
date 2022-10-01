dc12f40caf39c5aae9f78c08ae987647d346a08c
doyun <kjb119912@gmail.com>
2022-10-01 14:53:33 +0900

    숨고 클론 앱 시작

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Clone <a href="https://nextjs.org">Soomgo App!</a>
         </h1>
 
         <p className={styles.description}>
