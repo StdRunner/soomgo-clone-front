b5404fd217e17e41db52a865866ef1ca3ff097b2
doyun <kjb119912@gmail.com>
2022-11-09 21:59:18 +0900

    - Navbar 스타일 변경

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,36 +5,42 @@ const Navbar: React.FC = () => {
     return (
         <div className='flex fixed top-0 left-0 w-full h-16 bg-white '>
             <div className='flex w-full px-14 justify-center items-center'>
-                <div className='flex w-96 h-min pr-14 justify-evenly'>
-                    <a href="/">LOGO</a>
+                <div className='flex w-96 h-9 justify-evenly items-center'>
+                    <a href="/">
+                        <img 
+                            src="https://assets.cdn.soomgo.com/icons/icon-navi-logo.svg" 
+                            alt='Logo Image'
+                            className='h-6'
+                        />
+                    </a>
                     <div className='font-semibold'>
-                        <Link href='/'>
+                        <a href='/'>
                             고수매칭
-                        </Link>
+                        </a>
                     </div>
                     <div className='font-semibold'>
-                        <Link href='/'>
+                        <a href='/'>
                             고수찾기
-                        </Link>
+                        </a>
                     </div>
                     <div className='font-semibold'>
-                        <Link href='/'>
+                        <a href='/'>
                             마켓
-                        </Link>
+                        </a>
                     </div>
                     <div className='font-semibold'>
-                        <Link href='/'>
+                        <a href='/'>
                             커뮤니티
-                        </Link>
+                        </a>
                     </div>
                 </div>
-                <div className='w-72 h-min'>
+                <div className='w-72 h-min pl-10'>
                     <input type="text" 
                         placeholder='어떤 서비스가 필요하세요?'
                         className='w-full h-10 px-3 bg-[#f4f4f4] text-sm rounded-md'
                     />
                 </div>
-                <div className='flex w-72 pl-14 justify-evenly text-sm items-center'>
+                <div className='flex w-72 pl-10 justify-evenly text-sm items-center'>
                     <Link href='/login'>
                         로그인
                     </Link>
