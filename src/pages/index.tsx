4b337bad501ff363e48f2f65d4bd9bf3230253c0
김도윤 <gm4gitlab@gmail.com>
2022-11-09 17:58:44 +0900

    - NavBar 추가 - 로그인, 회원가입 페이지 추가 - tailwindcss, classnames 모듈 추가 및 사용

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    return (
+        <div className=''>
+            <div className='w-full h-full'>
+                Soomgo Clone App Home
+            </div>
+        </div>
+    )
+}
+
+export default Home
