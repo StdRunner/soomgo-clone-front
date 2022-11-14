e38c4bff5ff0841d60674479dbaad56ff9b11a19
김도윤 <gm4gitlab@gmail.com>
2022-11-14 18:27:29 +0900

    - Footer 스타일 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,13 @@ const Footer = () => {
   return (
     <div className='h-96 bg-white border-t border-[#e1e1e1] pt-5 pb-10 justify-center items-center'>
         <div className='h-full mx-96 px-20'>
-            <div className='h-1/2'>
-                <div className='h-full w- mb-5'>
-                    FOOTER 1-1
+            <div className='flex h-1/2'>
+                <div className='h-full w-72'>
+                    <div className='mb-7'>
+                        FOOTER 1-1
+                    </div>
                 </div>
-                <div>
+                <div className='h-full w-96'>
                     FOOTER 1-2
                 </div>
             </div>
